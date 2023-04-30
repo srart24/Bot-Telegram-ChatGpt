75f447f2e356fb9cdd56dc4ef44ce9d62a03c1ba
srart24࿐ <86997468+srart24@users.noreply.github.com>
2023-04-30 08:26:54 +0700

    Create setting.js

---
+++ setting/setting.js
@@ -0,0 +1,15 @@
+/*
+=========
+Author: @srart24
+=========
+*/
+
+
+
+// TOKEN BOT
+global.botToken = "Isi Dengan Token Bot"
+
+// OPEN AI
+
+global.KeyGpt = "isi apikey openai"
+global.Org = "isi organisasi openai"
